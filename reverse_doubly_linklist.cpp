9855125cbc0aa4a605a8cb6956a94ad1ed1919f9
rizwanhossainrahat <rizwanhossainrahat400@gmail.com>
2023-09-23 18:02:51 +0600

    corner case handle

--- reverse_doubly_linklist.cpp
+++ reverse_doubly_linklist.cpp
@@ -43,19 +43,25 @@ void reverse(Node* head,Node* tail)
         i=i->next;
         j=j->prev;
     }
-
+     swap(i->val,j->val);
 }
 int main()
 {
     Node* head=new Node(10);   
     Node* a=new Node(20);   
     Node* b=new Node(30); 
+    // Node* c=new Node(40); 
+    // Node* tail=c;  
     Node* tail=b;  
 
     head->next=a;
     a->prev=head;
     a->next=b;
     b->prev=a;
+    b->next=NULL;
+    // b->next=c;
+    // c->prev=b;
+    // c->next=NULL;
     reverse(head,tail);
     print_normal(head);
     
